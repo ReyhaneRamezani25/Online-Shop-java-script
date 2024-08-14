b6568c43fec17a36852a548803fd9b0d2812afa6
ramezanir25121381 <reyhaneramezani11@gmail.com>
2024-08-14 19:03:47 +0330

    product.js added

---
+++ product.js
@@ -0,0 +1,44 @@
+export const productsData = [
+    {
+        id: 1,
+        title: "Army Bomb",
+        price: 10.99,
+        imgURL : "./images/product1.jpg"
+    },
+
+    {
+        id: 2,
+        title: "Stray kids Bomb",
+        price: 10.99,
+        imgURL : "./images/product2.jpg"
+    },
+
+    {
+        id: 3,
+        title: "EXO light stick",
+        price: 10.99,
+        imgURL : "./images/product3.jpg"
+    },
+
+    {
+        id: 4,
+        title: "TXT light stick",
+        price: 10.99,
+        imgURL : "./images/product4.jpg"
+    },
+
+    {
+        id: 5,
+        title: "Jungkook album",
+        price: 10.99,
+        imgURL : "./images/product5.jpg"
+    },
+
+    {
+        id: 6,
+        title: "EXO hoodie",
+        price: 10.99,
+        imgURL : "./images/product6.jpg"
+    },
+
+]
